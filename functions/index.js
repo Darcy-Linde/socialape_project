47459d58a09b8f67dc5a7cdbfff7fa1206c23415
Darcy Linde <lindedarcy@gmail.com>
2019-06-25 19:48:47 -0400

    added signup route with user auth. Tested with postman

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,22 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-admin.initializeApp();
-
 const express = require("express");
 const app = express();
+admin.initializeApp();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDoo7OYiUGd79thb3elubO_yPj8_O1PQg",
+  authDomain: "socialape-52a85.firebaseapp.com",
+  databaseURL: "https://socialape-52a85.firebaseio.com",
+  projectId: "socialape-52a85",
+  storageBucket: "socialape-52a85.appspot.com",
+  messagingSenderId: "52039374948",
+  appId: "1:52039374948:web:7ebd73e737a3a0a7"
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
 
 app.get("/screams", (req, res) => {
   admin
@@ -44,6 +56,31 @@ app.post("/scream", (req, res) => {
     .catch(err => {
       res.status(500).json({ error: "Something went wrong" });
       console.error(err);
+    });
+});
+
+// Signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  // TODO: validate data
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res
+        .status(201)
+        .json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
     });
 });
 
